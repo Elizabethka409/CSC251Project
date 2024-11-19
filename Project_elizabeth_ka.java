42b3bf8b04acbd68c95c806b66b6c291c957e3dc
Elizabethka409 <133019223+Elizabethka409@users.noreply.github.com>
2024-11-19 12:46:24 -0500

    Updated demo file to work and show information to console
    Added input.nextLine() under age to remove buffer; fixed other issues such as adding another if statement to read for buffer after each policy in the text file, code to read height line of information from text file which I had forgotten to add, and ideal spacing and placement within to string information of demo file to make text readable to user.

--- Project_elizabeth_ka.java
+++ Project_elizabeth_ka.java
@@ -28,13 +28,17 @@ public class Project_elizabeth_ka {
          first_name = input.nextLine();
          last_name = input.nextLine();
          age = input.nextInt();
+         input.nextLine();
          smoking_status = input.nextLine();
+         height = input.nextDouble();
          weight = input.nextDouble();
-                  
+                 
          // to skip blank line in text file    
-         if (input.hasNext()) {
+         if (input.hasNext())
             line = input.nextLine();
-         }
+         if (input.hasNext())
+            line = input.nextLine();
+         
          // creating policy object
          // demo = name of policy
          Policy demo = new Policy(policy_number, provider_name, first_name, last_name, age, smoking_status, height, weight);
@@ -51,18 +55,17 @@ public class Project_elizabeth_ka {
             System.out.println("Policyholder's First Name: " + demo.getFirstName());
             System.out.println("Policyholder's Last Name: " + demo.getLastName());
             System.out.println("Policyholder's Age: " + demo.getAge());
-            System.out.println("Policyholder's Smoking Status (Y/N): " + demo.getStatus());
+            System.out.println("Policyholder's Smoking Status: " + demo.getStatus());
             System.out.println("Policyholder's Height: " + demo.getHeight() + " inches");
             System.out.println("Policyholder's Weight: " + demo.getWeight() + " pounds");
             System.out.printf("Policyholder's BMI: %.2f\n", demo.calcBMI());
-            System.out.printf("Policy Price: $%.2f\n", demo.calcInsurancePolicy());
+            System.out.printf("Policy Price: $%.2f\n\n", demo.calcInsurancePolicy());
       
             if(demo.getStatus().equalsIgnoreCase("smoker")) { // to keep track of the number of smokers 
                numOfSmokers++;
          }
-         
-         System.out.print("\nThe number of policies with a smoker is: " + numOfSmokers);
+      }   
+         System.out.print("The number of policies with a smoker is: " + numOfSmokers);
          System.out.print("\nThe number of policies with a non-smoker is: " + (PolicyList.size() - numOfSmokers));
-      }  
    }
 }
